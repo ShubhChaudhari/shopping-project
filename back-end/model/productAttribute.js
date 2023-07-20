b802e68e4813ab551740d098b7524631e6621d9d
Prashant Doshi <prashantdoshi@2c2pexternal.com>
2023-07-20 18:59:50 +0530

    backend

--- back-end/model/productAttribute.js
+++ back-end/model/productAttribute.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const productAttributeSchema = new mongoose.Schema({
     productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
     productSize: { type: String, required: true },
-    productColor: { type: String, required: true },
+    productColour: { type: String, required: true },
 });
 
 
